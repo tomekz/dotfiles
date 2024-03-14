623bdf7bafa13dd3b5934fc5277f15d1e89ef3ff
tomekz <zadrozny.tomasz@gmail.com>
2024-03-14 13:53:10 +0100

    fix obsidian config

--- nvim/.config/nvim/lua/user/obsidian.lua
+++ nvim/.config/nvim/lua/user/obsidian.lua
@@ -29,7 +29,7 @@ obsidian.setup {
         -- Where to put new notes created from completion. Valid options are
         --  * "current_dir" - put new notes in same directory as the current buffer.
         --  * "notes_subdir" - put new notes in the default notes subdirectory.
-        new_notes_location = "~/Documents/obisidian/work/0 Inbox",
+        new_notes_location = "current_dir",
 
         -- Control how wiki links are completed with these (mutually exclusive) options:
         --
